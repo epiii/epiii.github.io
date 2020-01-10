38513ee807193130fd61b17c8422640cbcfc6af1
epiii <elfri.app@gmail.com>
2020-01-09 20:12:44 -0800

    Updates

---
+++ precache-manifest.019d98d86dd330cd689a9eea50e24815.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c2c0624eaef7a804fe5c8ec8fc8105c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0bdc3e8afd8fd71e1d5",
+    "url": "/static/css/2.c7b90ac4.chunk.css"
+  },
+  {
+    "revision": "61e9b2de1662061a3d31",
+    "url": "/static/css/main.a12feb68.chunk.css"
+  },
+  {
+    "revision": "e0bdc3e8afd8fd71e1d5",
+    "url": "/static/js/2.e23cfd8f.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/static/js/2.e23cfd8f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "61e9b2de1662061a3d31",
+    "url": "/static/js/main.d071b038.chunk.js"
+  },
+  {
+    "revision": "736aa680929306c29166",
+    "url": "/static/js/runtime-main.b5403b0a.js"
+  }
+]);
